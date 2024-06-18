38c20a1665cc2c520518497e7750362220bcb2c9
mohamed-ra-username <163635020+mohamed-ra-username@users.noreply.github.com>
2024-06-18 18:51:08 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="styles.css">
+
+<html lang="en">
+
+    <?php
+        include "database.php";
+
+        function Display_Student($name)
+        {
+            $student_name = ucfirst($name);
+            echo ' Student: ';
+            echo '<div class="student" >'. $student_name .'</div>';
+            echo '<br>';
+        }
+
+        function Show_Students($array){
+            foreach($array as $name)
+            {
+                Display_Student($name);
+            }
+        }
+    ?>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+
+    <body>
+        <h1>-My website-</h1>
+        <?php
+            $user ="Admin";
+            if ($user == "Admin") {
+                echo "Mr admin";
+            } else {
+                echo "Student";
+            }?>
+
+        <h2>Status:</h2>
+        
+        <?php
+            Show_Students($students)
+        ?>
+    </body>
+</html>
