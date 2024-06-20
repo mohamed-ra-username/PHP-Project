efa966e6252b69c21a1072063ba4a404b5209ace
Mohammed Elsaeed <163653092+WillowGrief@users.noreply.github.com>
2024-06-20 14:46:55 +0000

    Co-authored-by: Cheese <lostmysmile@users.noreply.github.com>

--- database.php
+++ database.php
@@ -4,7 +4,7 @@
         ["name" => "Mohammed Elsaeed" , "gpa" => 3.5, "email" => "gibberesh@gmail.com"],
         ["name" => "ahmed"   , "gpa" => 5, "email" => "shinycapybara@gmail.com"],
         ["name" => "amr nader"     , "gpa" => 2.0, "email" => "lolatnight@gmail.com"],
-        ["name" => "mahmoud abdelgawad"    , "gpa" => 3, "email" => "g"]
+        ["name" => "mahmoud abdelgawad"    , "gpa" => 3, "email" => "gagag@gmail.com"]
     ];
     $courses = ["english","math","science"];
 ?>
