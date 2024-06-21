661adcbbe1eaa4982d73bf17aae3d2241744fadf
mohamed-ra-username <163635020+mohamed-ra-username@users.noreply.github.com>
2024-06-21 20:35:36 +0300

    Update database.php

--- database.php
+++ database.php
@@ -62,9 +62,9 @@
     // }
 
     $dbServername = "localhost";
-    $dbUsername = "id22349109_localhost";
-    $dbPassword = "54123Ws@";
-    $dbName = "id22349109_test";
+    $dbUsername = "test";
+    $dbPassword = "@";
+    $dbName = "students";
     
     $conn= mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
     
