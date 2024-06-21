5b418aeb0889f5765557a81c85f621d6477f5ce3
mohamed-ra-username <163635020+mohamed-ra-username@users.noreply.github.com>
2024-06-21 22:48:24 +0300

    Update students.php

--- students.php
+++ students.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="table+.css">
+    <link rel="stylesheet" href="table.css">
     <link rel="stylesheet" href="buttons.css">
 
     <meta charset="UTF-8">
@@ -42,28 +42,23 @@
 
             <tbody>
                 <?php
-                    while ($row = mysqli_fetch_assoc($all_students)) {
+                    while ($row = mysqli_fetch_assoc($all_students)) 
+                    {
+                        //defination
                         $id = $row['id'] ;
                         $name = $row['name'] ;
                         $gpa = $row['gpa'] ;
                         $email = $row['email'] ;
                         $owned_courses = $row["owned_courses"];
-                    }
-                ?>
-
-                <tr>
-                    <?php
-                        if ($user == "Admin"){
-                            echo '<td class = "admin-panel">';
-                            echo '<button class = "button btn-info" type="button">EDIT</button>';
-                            echo '<button class = "button btn-danger" type="button">DELETE</button>';
-                            echo '</td>';
-                        }
-                        echo "<td class = 'small'>" . ++$current_row . "</td>";
-                            echo "<td class = 'name'> $name</td>";
-                            echo "<td class = 'email'> $email</td>";
-                            echo "<td class = 'small'> $gpa</td>";
-                            
+                        // row details
+                        echo '<tr>';
+                            if ($user == "Admin"){
+                                echo '<td class = "admin-panel">';
+                                echo '<button class = "button btn-info" type="button">EDIT</button>';
+                                echo '<button class = "button btn-danger" type="button">DELETE</button>';
+                                echo '</td>';
+                            }
+                            echo "<td class = 'small'>" . ++$current_row . "</td> <td class = 'name'> $name</td> <td class = 'email'> $email</td> <td class = 'small'> $gpa</td>";
                             if ($user == "Admin")
                             {
                                 echo '<td>';
@@ -77,18 +72,19 @@
                                         $id = intval($idx);
                                         $courses = mysqli_query($conn, "SELECT title FROM courses where id=$id;");
                                         $course = mysqli_fetch_assoc($courses);
-    
+                                        
                                         echo $course ? $course["title"] : "<err> Course Not Found </err>";
-    
+                                        
                                         if (++$i < count($owned_courses))
                                         echo ", ";
                                         
                                     }
                                 }
-                                }
-                                ?>
-                                </td>
-                    </tr>
+                                    echo '</td>';
+                            }
+                        echo '</tr>';
+                    }
+                ?>
             </tbody>
             <?php
                 if ($user == "Admin"){
