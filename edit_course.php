f71a4666f4d724ea45620234d10f8434c4becd75
mohamed-ra-username <163635020+mohamed-ra-username@users.noreply.github.com>
2024-07-02 00:26:22 +0300

    fixed sites' title
    renamed sties  from "document" to their corresponding name instead

--- edit_course.php
+++ edit_course.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="css/create.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Entries</title>
+    <title>Edit Course</title>
 </head>
 <body>
     <div class="container">
